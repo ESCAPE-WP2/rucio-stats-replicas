fd5f1660c7a3b31fdb7cf40dea977aec70cf608b
albavemo <albavemo11@gmail.com>
2021-11-11 10:41:27 +0100

    Changed conn to session

--- interconnection.py
+++ interconnection.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 from rucio.db.sqla import session
 
-conn = session.get_session()
+session = session.get_session()
 
 # Experiments names
 experiments = [
@@ -30,7 +30,7 @@ def _post_to_es(es_url, data_dict):
 
 # Func get QoS for each RSE
 def get_qos(rse):
-	query_get_qos = conn.execute("SELECT VALUE a FROM RSE_ATTR_MAP a INNER JOIN RSES r ON r.id=a.rse_id AND a.KEY='QOS' AND r.RSE='"+rse+"'").fetchone()
+	query_get_qos = session.execute("SELECT VALUE a FROM RSE_ATTR_MAP a INNER JOIN RSES r ON r.id=a.rse_id AND a.KEY='QOS' AND r.RSE='"+rse+"'").fetchone()
 	if not query_get_qos:
 		rse_qos = "NULL"
 		return(rse_qos)
@@ -40,12 +40,12 @@ def get_qos(rse):
 
 # Main func
 def get_replicas(push_to_es=False, es_url=None):
-	query_get_scopes = conn.execute("SELECT SCOPE FROM SCOPES")
+	query_get_scopes = session.execute("SELECT SCOPE FROM SCOPES")
 	scopes_list = []
 	for row in query_get_scopes:
 		scopes_list.append(row[0])
 
-	query_get_rses = conn.execute("SELECT RSE FROM RSES")
+	query_get_rses = session.execute("SELECT RSE FROM RSES")
 	rses_list = []
 	for row in query_get_rses:
 		rses_list.append(row[0])
@@ -56,14 +56,14 @@ def get_replicas(push_to_es=False, es_url=None):
 			rse_qos = get_qos(rse)
 			
 			#Get amount replicas per RSE
-			tReplicas = conn.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
-			tReplicas_bytes = conn.execute("SELECT SUM(rep.bytes) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
+			tReplicas = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
+			tReplicas_bytes = session.execute("SELECT SUM(rep.bytes) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
 
 			# Replica state
-			tReplicasA = conn.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"' AND state='A'")
-			tReplicasB = conn.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='B' AND scope='"+scope+"' AND rse='"+rse+"' AND state='B'")
-			tReplicasC = conn.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='C' AND scope='"+scope+"' AND rse='"+rse+"' AND state='C'")
-			tReplicasU = conn.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='U' AND scope='"+scope+"' AND rse='"+rse+"' AND state='U'")
+			tReplicasA = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"' AND state='A'").fetchone()
+			tReplicasB = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='B' AND scope='"+scope+"' AND rse='"+rse+"' AND state='B'").fetchone()
+			tReplicasC = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='C' AND scope='"+scope+"' AND rse='"+rse+"' AND state='C'").fetchone()
+			tReplicasU = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='U' AND scope='"+scope+"' AND rse='"+rse+"' AND state='U'").fetchone()
 
 			# Check if there are replicas:
 			if tReplicas[0] > 0:
@@ -97,23 +97,22 @@ def get_replicas(push_to_es=False, es_url=None):
 
 
 def get_dids(push_to_es=False, es_url=None):
-	#query_dids = conn.execute("SELECT COUNT(*) FROM dids WHERE availability='A'")
-	#query_dids_bytes = conn.execute("SELECT SUM(bytes) FROM dids WHERE availability='A'")
+	#query_dids = session.execute("SELECT COUNT(*) FROM dids WHERE availability='A'")
+	#query_dids_bytes = session.execute("SELECT SUM(bytes) FROM dids WHERE availability='A'")
 	#tDids = query_dids.first()[0]
 	#tDids_bytes = query_dids_bytes.first()[0]
 	#print(tDids)
 	#print(tDids_bytes)
 
-	query_f = conn.execute("SELECT count(*) FROM dids WHERE did_type='F' AND availability='A'")
-	query_d = conn.execute("SELECT count(*) FROM dids WHERE did_type='D' AND availability='A'")
-	query_c = conn.execute("SELECT count(*) FROM dids WHERE did_type='C' AND availability='A'")
+	query_f = session.execute("SELECT count(*) FROM dids WHERE did_type='F' AND availability='A'")
+	query_d = session.execute("SELECT count(*) FROM dids WHERE did_type='D' AND availability='A'")
+	query_c = session.execute("SELECT count(*) FROM dids WHERE did_type='C' AND availability='A'")
 
 	tFiles = query_f.first()[0]
 	tDatasets = query_d.first()[0]
 	tContainers = query_c.first()[0]
 
 	if push_to_es:
-		print("PUSH DIDS INFO")
 		rucio_dids_stats = {}
 		rucio_dids_stats["producer"] = "escape_wp2"
 		rucio_dids_stats["type"] = "alba_dids_stats"
