74f355f2b2ca97852798696e8d04f5a70c4d4b82
albavemo <albavemo11@gmail.com>
2021-11-16 10:10:15 +0100

    Protected functionality

--- interconnection.py
+++ interconnection.py
@@ -7,12 +7,11 @@ import time
 import requests
 import argparse
 from datetime import datetime
-
 from rucio.db.sqla import session
 
 session = session.get_session()
 
-# Experiments names
+# Global experiment info
 experiments = [
     "SKA", "LSST", "CTA", "LOFAR", "MAGIC", "ATLAS", "FAIR", "CMS", "VIRGO"
 ]
@@ -49,21 +48,29 @@ def get_replicas(push_to_es=False, es_url=None):
 	rses_list = []
 	for row in query_get_rses:
 		rses_list.append(row[0])
-
+	
 	for scope in scopes_list:
 		rse_found, rse_found_bytes = {}, {}
+
 		for rse in rses_list:
 			rse_qos = get_qos(rse)
-			
 			#Get amount replicas per RSE
 			tReplicas = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
 			tReplicas_bytes = session.execute("SELECT SUM(rep.bytes) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"'").fetchone()
 
-			# Replica state
-			tReplicasA = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='A' AND scope='"+scope+"' AND rse='"+rse+"' AND state='A'").fetchone()
-			tReplicasB = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='B' AND scope='"+scope+"' AND rse='"+rse+"' AND state='B'").fetchone()
-			tReplicasC = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='C' AND scope='"+scope+"' AND rse='"+rse+"' AND state='C'").fetchone()
-			tReplicasU = session.execute("SELECT COUNT(*) FROM replicas rep INNER JOIN rses r ON rep.rse_id=r.id WHERE state='U' AND scope='"+scope+"' AND rse='"+rse+"' AND state='U'").fetchone()
+			# Protected Replicas:
+			tRules = session.execute("SELECT COUNT(*) FROM rules ru INNER JOIN replicas rep ON ru.scope=rep.scope AND ru.name=rep.name INNER JOIN rses r ON rep.rse_id=r.id AND rep.state='A' AND ru.state='O' AND ru.scope='"+scope+"' AND r.rse='"+rse+"'").fetchone()
+			tRules_bytes = session.execute("SELECT SUM(rep.bytes) FROM replicas rep INNER JOIN rules ru ON ru.scope=rep.scope AND ru.name=rep.name INNER JOIN rses r ON rep.rse_id=r.id AND rep.state='A' AND rep.scope='"+scope+"' AND r.rse='"+rse+"'").fetchone()
+
+			#Not protected Replicas:
+			tNoRules = tReplicas[0]-tRules[0]
+			if tReplicas_bytes[0] is None:
+				tNoRules_bytes = 0
+			else:
+				if tRules_bytes[0] is None:
+					tNoRules_bytes = tReplicas_bytes[0]
+				else:
+					tNoRules_bytes = tReplicas_bytes[0]-tRules_bytes[0]
 
 			# Check if there are replicas:
 			if tReplicas[0] > 0:
@@ -75,6 +82,7 @@ def get_replicas(push_to_es=False, es_url=None):
 						experiment_name = experiment
 
 				# Replica info push
+				# Protected replicas
 				if push_to_es:
 					rucio_rep_stats = {}
 					rucio_rep_stats["producer"] = "escape_wp2"
@@ -85,52 +93,33 @@ def get_replicas(push_to_es=False, es_url=None):
 					rucio_rep_stats["qos"] = rse_qos
 					rucio_rep_stats["experiment"] = experiment_name
 					
-					rucio_rep_stats["total_replicas"] = tReplicas[0]	
-					rucio_rep_stats["total_replicas_bytes"] = tReplicas_bytes[0]
-
-					rucio_rep_stats["total_replicasA"] = tReplicasA[0]
-					rucio_rep_stats["total_replicasB"] = tReplicasB[0]
-					rucio_rep_stats["total_replicasC"] = tReplicasC[0]
-					rucio_rep_stats["total_replicasU"] = tReplicasU[0]
+					rucio_rep_stats["protectedVar"] = "protected"
+					rucio_rep_stats["total_replicas"] = tRules[0]	
+					rucio_rep_stats["total_replicas_bytes"] = tRules_bytes[0]
 
 					_post_to_es(es_url, rucio_rep_stats)
 
+				#Not protected replicas
+				if push_to_es:
+					rucio_rep_stats = {}
+					rucio_rep_stats["producer"] = "escape_wp2"
+					rucio_rep_stats["type"] = "alba_rep_stats"
+					rucio_rep_stats["timestamp"] = int(time.time())
+					rucio_rep_stats["scope"] = scope
+					rucio_rep_stats["rse"] = rse
+					rucio_rep_stats["qos"] = rse_qos
+					rucio_rep_stats["experiment"] = experiment_name
+					
+					rucio_rep_stats["protectedVar"] = "not protected"
+					rucio_rep_stats["total_replicas"] = tNoRules
+					rucio_rep_stats["total_replicas_bytes"] = tNoRules_bytes
+	
+					_post_to_es(es_url, rucio_rep_stats)
 
-def get_dids(push_to_es=False, es_url=None):
-	#query_dids = session.execute("SELECT COUNT(*) FROM dids WHERE availability='A'")
-	#query_dids_bytes = session.execute("SELECT SUM(bytes) FROM dids WHERE availability='A'")
-	#tDids = query_dids.first()[0]
-	#tDids_bytes = query_dids_bytes.first()[0]
-	#print(tDids)
-	#print(tDids_bytes)
-
-	query_f = session.execute("SELECT count(*) FROM dids WHERE did_type='F' AND availability='A'")
-	query_d = session.execute("SELECT count(*) FROM dids WHERE did_type='D' AND availability='A'")
-	query_c = session.execute("SELECT count(*) FROM dids WHERE did_type='C' AND availability='A'")
-
-	tFiles = query_f.first()[0]
-	tDatasets = query_d.first()[0]
-	tContainers = query_c.first()[0]
-
-	if push_to_es:
-		rucio_dids_stats = {}
-		rucio_dids_stats["producer"] = "escape_wp2"
-		rucio_dids_stats["type"] = "alba_dids_stats"
-		rucio_dids_stats["timestamp"] = int(time.time())
-		#rucio_dids_stats["total_dids"] = tDids
-		#rucio_dids_stats["total_dids_bytes"] = tDids_bytes	
-
-		rucio_dids_stats["total_files"] = tFiles
-		rucio_dids_stats["total_datasets"] = tDatasets
-		rucio_dids_stats["total_containers"] = tContainers
-						
-		_post_to_es(es_url, rucio_dids_stats)
 
 def main():
 	parser = argparse.ArgumentParser(description="Rucio Stats Interconnection Probe")
-
 	parser.add_argument("--push",required=False,action='store_true',default=False,help="Push to an ES datasource")
-
 	parser.add_argument("--url",required=False,dest='es_url',help="ES datasource url")
 
 	args = parser.parse_args()
@@ -143,7 +132,6 @@ def main():
 	it = datetime.now()
 	init_time = it.strftime("%H:%M:%S")
 	print("Current Time =", init_time)
-	get_dids(push_to_es,es_url)
 	get_replicas(push_to_es,es_url)
 
 	et = datetime.now()
